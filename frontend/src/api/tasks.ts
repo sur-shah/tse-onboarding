7ca2bd37183c26edbbf3e62738c6f0294ee84807
sur-shah <sushah@ucsd.edu>
2025-01-01 22:23:25 -0800

    Implement part 1.1

--- frontend/src/api/tasks.ts
+++ frontend/src/api/tasks.ts
@@ -94,3 +94,14 @@ export async function getTask(id: string): Promise<APIResult<Task>> {
     return handleAPIError(error);
   }
 }
+
+export async function getAllTasks(): Promise<APIResult<Task[]>> {
+  try {
+    const response = await get("/api/tasks")
+    const json = (await response.json()) as TaskJSON[];
+    const tasks = json.map(parseTask);
+    return { success: true, data: tasks };
+  } catch (error) {
+    return handleAPIError(error);
+  }
+}
---
+++ frontend/src/components/TaskList.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import { getAllTasks, type Task } from "src/api/tasks";
+import { TaskItem } from "src/components";
+import styles from "src/components/TaskList.module.css";
+
+export interface TaskListProps {
+  title: string;
+}
+
+export function TaskList({ title }: TaskListProps) {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  useEffect(() => {
+    console.log("Fetching tasks...")
+    getAllTasks()
+        .then((result) => {
+            if(result.success){
+                console.log("Tasks fetched: " + result.data)
+                setTasks(result.data)
+            } else{
+                console.log("An error occured: " + result.error)
+                alert(result.error)
+            }
+        })
+        .catch((reason) => alert(reason));
+  }, []);
+
+  return (
+    <div className = {styles.list}>
+      <span className = {styles.title}>{title}</span>
+      <div className = {styles.item}>
+        {tasks.length === 0 ? (
+         <p>No tasks yet. Add one above to get started.</p>
+        ) : tasks.map((task) => (
+          <TaskItem key = {task._id} task = {task}/>
+        ))}
+      </div>
+    </div>
+  );
+}
